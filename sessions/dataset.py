cb4a8a5009673645ecd1f8dd3000b6fc96f5b0b4
selenado <sc15556@student.samk.fi>
2026-03-18 22:59:44 +0200

    Update dataset.py

--- sessions/dataset.py
+++ sessions/dataset.py
@@ -1,6 +1,8 @@
 import torch
 from PIL import Image
 from torchvision.transforms.functional import to_tensor
+from args import get_args
+from utils import resize_box_xyxy
 
 
 class ObjDetectionDataset(torch.utils.data.Dataset):
@@ -11,22 +13,29 @@ class ObjDetectionDataset(torch.utils.data.Dataset):
         return len(self.df)
 
     def __getitem__(self, idx):
+        args = get_args()
         # TODO 1: Get the row number idx from dataframe
         # your code here
-        row = self.df.ilock[idx]
+        row = self.df.iloc[idx]
 
-        img = Image.open(row["image_path"]).convert("RGB")
+        img = Image.open(row["images"]).convert("RGB")
         w, h = img.size
+        
+        image = img.resize((args.image_size, args.image_size))
+        
         image = to_tensor(img)
 
         boxes, labels = [], []
-        with open(row["label_path"]) as f:
+        with open(row["labels"]) as f:
             for line in f:
                 cls, xc, yc, bw, bh = map(float, line.split())
                 x1 = (xc - bw/2) * w
                 y1 = (yc - bh/2) * h
                 x2 = (xc + bw/2) * w
                 y2 = (yc + bh/2) * h
+                
+                x1, y1, x2, y2 = resize_box_xyxy((x1, y1, x2, y2), w, h, args.image_size, args.image_size)
+                
                 boxes.append([x1, y1, x2, y2])
                 labels.append(int(cls) + 1)
 
