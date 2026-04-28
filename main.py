000c7ae9f59e6266e30340e79baf462fe26eef25
selenado <sc15556@student.samk.fi>
2026-04-28 11:10:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import DataLoader
 from model import build_model
 from trainer import train_model
+from augmentations import build_train_transforms, build_val_transforms
 
 def collate(batch):
     images, targets = zip(*batch)
@@ -19,12 +20,26 @@ def main():
     val_df   = pd.read_csv(os.path.join(args.csv_dir, "val_df.csv"))
     
     # 2. Prepare datasets
-    train_dataset = ObjDetectionDataset(train_df)
-    val_dataset   = ObjDetectionDataset(val_df)
+    train_dataset = ObjDetectionDataset(train_df, transform=build_train_transforms(args.image_size))
+    val_dataset   = ObjDetectionDataset(val_df, transform=build_val_transforms(args.image_size))
     
     # 3. Create data loaders
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate, num_workers=0, pin_memory=torch.cuda.is_available())
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate, num_workers=0, pin_memory=torch.cuda.is_available())
+    train_loader = DataLoader(
+        train_dataset, 
+        batch_size=args.batch_size, 
+        shuffle=True, 
+        collate_fn=collate, 
+        num_workers=8, 
+        pin_memory=torch.cuda.is_available()
+        )
+    val_loader = DataLoader(
+        val_dataset, 
+        batch_size=args.batch_size, 
+        shuffle=False, 
+        collate_fn=collate, 
+        num_workers=8, 
+        pin_memory=torch.cuda.is_available()
+        )
     
     images, targets = next(iter(train_loader))
     
@@ -42,3 +57,4 @@ def main():
     
 if __name__ == '__main__':
     main()
+    
